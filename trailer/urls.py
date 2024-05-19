ab6cc78cba47cd98d331b5815fd332f8e7dba5fe
Tegar Wahyu <tegarwahyu107@gmail.com>
2024-05-19 17:48:35 +0700

    feat: episode detail

--- trailer/urls.py
+++ trailer/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('search/', views.search_trailer, name='search_trailer'),
     path('film/<str:film_id>', views.film_detail, name='film_detail'),
     path('series/<str:series_id>', views.series_detail, name='series_detail'),
-    path('episode/<str:episode_id>', views.episode_detail, name='episode_detail'),
+    path('episode/<str:id>/<str:sub_judul>', views.episode_detail, name='episode_detail'),
 ]
